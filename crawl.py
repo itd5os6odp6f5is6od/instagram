ca1e103e6a40b56676bade9d47f7e50c73ce31a9
itd5os6odp6f5is6od <159675456+itd5os6odp6f5is6od@users.noreply.github.com>
2024-02-17 17:15:43 -0600

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -38,8 +38,7 @@ def crawl_url(url, target_patterns):
 
 if __name__ == "__main__":
     # Replace https://guns.lol/instagram with the URL you want to crawl
-    base_url = '
-    https://guns.lol/instagram
+    base_url =' 'https://guns.lol/instagram'
     
     target_patterns = ['/download']
 
